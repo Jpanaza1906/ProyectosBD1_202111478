897e123cb4e224a3f194f02f7ae2f90bdf756bbb
Jpanaza1906 <3848221850101@ingenieria.usac.edu.gt>
2024-02-28 19:43:18 -0600

    procesos_completo

--- Proyecto1/server/util/cargarTablas.js
+++ Proyecto1/server/util/cargarTablas.js
@@ -11,8 +11,8 @@ async function cargarTablas(csvFilePaths) {
         const connection = await oracledb.getConnection(dbConfig);
 
         //Lista de tablas deseadas
-        const tablasDeseadas = ['ORDEN'];
-        //const tablasDeseadas = ['PAIS', 'CATEGORIA', 'CLIENTE', 'VENDEDOR', 'PRODUCTO', 'ORDEN'];
+        //const tablasDeseadas = ['ORDEN'];
+        const tablasDeseadas = ['PAIS', 'CATEGORIA', 'CLIENTE', 'VENDEDOR', 'PRODUCTO', 'ORDEN'];
         //const tablasDeseadas = ['PAIS', 'CATEGORIA', 'CLIENTE', 'VENDEDOR', 'PRODUCTO'];
 
         // recorrer las tablas deseadas
@@ -43,8 +43,11 @@ async function cargarTablas(csvFilePaths) {
                                 //const cleanedValue = value.replace(/'/g, "''");
 
                                 //si viene un valor de fecha tipo 20-02-2024 o 1-03-2024, devolverlo de esta manera TO_DATE('fecha', 'DD-MM-YYYY') o 
-                                if ((cleanedValue.match(/\d{2}-\d{2}-\d{4}/)) || (cleanedValue.match(/\d{1}-\d{2}-\d{4}/))) {
+                                if (cleanedValue.match(/\d{2}-\d{2}-\d{4}/)) {
                                     return `TO_DATE('${cleanedValue}', 'DD-MM-YYYY')`;
+                                } else if (cleanedValue.match(/\d{1}-\d{2}-\d{4}/)) {
+                                    //agregar el 0 al dia
+                                    return `TO_DATE('0${cleanedValue}', 'DD-MM-YYYY')`;
                                 }
                                 return `'${cleanedValue}'`;
                             });
@@ -67,54 +70,6 @@ async function cargarTablas(csvFilePaths) {
                     });
 
             });
-
-            // Leer el archivo CSV y cargar los datos en la tabla con una sola consulta SQL
-            // const stream = fs.createReadStream(tablaFilePath);
-            // let registrosInsertados = []; // Almacena los registros insertados
-
-            // await new Promise((resolve, reject) => {
-            //     stream.pipe(csv({ separator: ';' }))
-            //         .on('data', async (row) => {
-            //             try {
-            //                 const values = Object.values(row).map(value => {
-            //                     // Envuelve todos los valores en comillas simples y realiza la limpieza necesaria
-            //                     const cleanedValue = value.replace(/'/g, '').replace(/\//g, '-');
-            //                     // Si es una fecha en formato DD-MM-YYYY, envuélvelo en TO_DATE
-            //                     if (cleanedValue.match(/\d{2}-\d{2}-\d{4}/)) {
-            //                         return `TO_DATE('${cleanedValue}', 'DD-MM-YYYY')`;
-            //                     }
-            //                     return `'${cleanedValue}'`;
-            //                 });
-            //                 registrosInsertados.push(values.join(',')); // Agrega los valores al conjunto de registros
-
-            //             } catch (error) {
-            //                 console.error('Error cargando la tabla', tabla, error);
-            //                 reject(error);
-            //             }
-            //         })
-            //         .on('end', async () => {
-            //             try {
-            //                 // Construye la consulta SQL con todos los registros insertados
-            //                 const sql = `INSERT INTO "SBD1P1"."${tabla}" VALUES (${registrosInsertados.join('),(')})`;
-            //                 await connection.execute(sql);
-            //                 console.log(`Tabla ${tabla} cargada exitosamente`);
-            //                 resolve();
-            //             } catch (error) {
-            //                 console.error('Error cargando la tabla', tabla, error);
-            //                 reject(error);
-            //             }
-            //         })
-            //         .on('error', (error) => {
-            //             console.error('Error cargando la tabla', tabla, error);
-            //             reject(error);
-            //         });
-            // });
-
-            // await connection.commit();
-
-
-
-
         }
         await connection.commit();
         await connection.close();
